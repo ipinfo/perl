b4541f5d8c32d3fb3bc174b44635ee96fc5e4594
abu usama <usamaliaqat5846@gmail.com>
2023-08-03 15:39:26 +0500

    updated  test and formatted

--- Geo-IPinfo/lib/Geo/Details.pm
+++ Geo-IPinfo/lib/Geo/Details.pm
@@ -5,121 +5,121 @@ use strict;
 use warnings;
 
 sub new {
-  my $class = shift;
-  my $data = shift;
-  my $key = shift // '';
+    my $class = shift;
+    my $data  = shift;
+    my $key   = shift // '';
 
-  # If $data is a hash reference, directly bless it into the class and return.
-  if (ref($data) eq 'HASH') {
-      bless $data, $class;
-      return $data;
-  }
+   # If $data is a hash reference, directly bless it into the class and return.
+    if ( ref($data) eq 'HASH' ) {
+        bless $data, $class;
+        return $data;
+    }
 
-  # If $data is a plain string, create a new hash reference and set the specified key to the string value.
-  # Use the provided key or default to ''.
-  my $self = { $key => $data };
-  bless $self, $class;
-  return $self;
+# If $data is a plain string, create a new hash reference and set the specified key to the string value.
+# Use the provided key or default to ''.
+    my $self = { $key => $data };
+    bless $self, $class;
+    return $self;
 }
 
 sub abuse {
-  return $_[0]->{abuse};
+    return $_[0]->{abuse};
 }
 
 sub ip {
-  return $_[0]->{ip};
+    return $_[0]->{ip};
 }
 
 sub org {
-  return $_[0]->{org};
+    return $_[0]->{org};
 }
 
 sub domains {
-  return $_[0]->{domains};
+    return $_[0]->{domains};
 }
 
 sub privacy {
-  return $_[0]->{privacy};
+    return $_[0]->{privacy};
 }
 
 sub timezone {
-  return $_[0]->{timezone};
+    return $_[0]->{timezone};
 }
 
 sub hostname {
-  return $_[0]->{hostname};
+    return $_[0]->{hostname};
 }
 
 sub city {
-  return $_[0]->{city};
+    return $_[0]->{city};
 }
 
 sub region {
-  return $_[0]->{region};
+    return $_[0]->{region};
 }
 
 sub country {
-  return $_[0]->{country};
+    return $_[0]->{country};
 }
 
 sub country_name {
-  return $_[0]->{country_name};
+    return $_[0]->{country_name};
 }
 
 sub country_flag {
-  return $_[0]->{country_flag};
+    return $_[0]->{country_flag};
 }
 
 sub country_flag_url {
-  return $_[0]->{country_flag_url};
+    return $_[0]->{country_flag_url};
 }
 
 sub country_currency {
-  return $_[0]->{country_currency};
+    return $_[0]->{country_currency};
 }
 
 sub continent {
-  return $_[0]->{continent};
+    return $_[0]->{continent};
 }
 
 sub is_eu {
-  return $_[0]->{is_eu};
+    return $_[0]->{is_eu};
 }
 
 sub loc {
-  return $_[0]->{loc};
+    return $_[0]->{loc};
 }
 
 sub latitude {
-  return $_[0]->{latitude};
+    return $_[0]->{latitude};
 }
 
 sub longitude {
-  return $_[0]->{longitude};
+    return $_[0]->{longitude};
 }
 
 sub postal {
-  return $_[0]->{postal};
+    return $_[0]->{postal};
 }
 
 sub asn {
-  return $_[0]->{asn};
+    return $_[0]->{asn};
 }
 
 sub company {
-  return $_[0]->{company};
+    return $_[0]->{company};
 }
 
 sub carrier {
-  return $_[0]->{carrier};
+    return $_[0]->{carrier};
 }
 
 sub meta {
-  return $_[0]->{meta};
+    return $_[0]->{meta};
 }
 
 sub all {
-  return $_[0];
+    return $_[0];
 }
 
 #-------------------------------------------------------------------------------
--- Geo-IPinfo/lib/Geo/IPinfo.pm
+++ Geo-IPinfo/lib/Geo/IPinfo.pm
@@ -12,379 +12,347 @@ use Geo::Details;
 use Net::CIDR::Lite;
 
 our $VERSION = '2.1.0';
-use constant DEFAULT_CACHE_MAX_SIZE => 4096;
-use constant DEFAULT_CACHE_TTL => 86_400;
-use constant DEFAULT_COUNTRY_FILE => 'countries.json';
-use constant DEFAULT_EU_COUNTRY_FILE => 'eu.json';
-use constant DEFAULT_COUNTRY_FLAG_FILE => 'flags.json';
+use constant DEFAULT_CACHE_MAX_SIZE        => 4096;
+use constant DEFAULT_CACHE_TTL             => 86_400;
+use constant DEFAULT_COUNTRY_FILE          => 'countries.json';
+use constant DEFAULT_EU_COUNTRY_FILE       => 'eu.json';
+use constant DEFAULT_COUNTRY_FLAG_FILE     => 'flags.json';
 use constant DEFAULT_COUNTRY_CURRENCY_FILE => 'currency.json';
-use constant DEFAULT_CONTINENT_FILE => 'continent.json';
-use constant DEFAULT_TIMEOUT => 2;
-use constant HTTP_TOO_MANY_REQUEST => 429;
+use constant DEFAULT_CONTINENT_FILE        => 'continent.json';
+use constant DEFAULT_TIMEOUT               => 2;
+use constant HTTP_TOO_MANY_REQUEST         => 429;
 
 my %valid_fields = (
-                    ip => 1,
-                    hostname => 1,
-                    city => 1,
-                    region => 1,
-                    country => 1,
-                    country_name => 1,
-                    country_flag_url => 1,
-                    loc => 1,
-                    latitude => 1,
-                    longitude => 1,
-                    org => 1,
-                    postal => 1,
-                    is_eu => 1,
-                    timezone => 1,
-                    geo => 1,
-                );
-my $base_url = 'https://ipinfo.io/';
+    ip               => 1,
+    hostname         => 1,
+    city             => 1,
+    region           => 1,
+    country          => 1,
+    country_name     => 1,
+    country_flag_url => 1,
+    loc              => 1,
+    latitude         => 1,
+    longitude        => 1,
+    org              => 1,
+    postal           => 1,
+    is_eu            => 1,
+    timezone         => 1,
+    geo              => 1,
+);
+my $base_url         = 'https://ipinfo.io/';
 my $country_flag_url = 'https://cdn.ipinfo.io/static/images/countries-flags/';
 
-my $cache_ttl = 0;
+my $cache_ttl    = 0;
 my $custom_cache = 0;
 
 #-------------------------------------------------------------------------------
 
-sub new
-{
-  my ($pkg, $token, %options) = @_;
+sub new {
+    my ( $pkg, $token, %options ) = @_;
 
-  my $self = {};
+    my $self = {};
 
-  $self->{base_url} = $base_url;
-  $self->{ua} = LWP::UserAgent->new;
-  $self->{ua}->ssl_opts('verify_hostname' => 0);
-  $self->{ua}->default_headers(HTTP::Headers->new(
-    Accept => 'application/json',
-    Authorization =>  'Bearer ' . $token
-  ));
-  $self->{ua}->agent("IPinfoClient/Perl/$VERSION");
+    $self->{base_url} = $base_url;
+    $self->{ua}       = LWP::UserAgent->new;
+    $self->{ua}->ssl_opts( 'verify_hostname' => 0 );
+    $self->{ua}->default_headers(
+        HTTP::Headers->new(
+            Accept        => 'application/json',
+            Authorization => 'Bearer ' . $token
+        )
+    );
+    $self->{ua}->agent("IPinfoClient/Perl/$VERSION");
 
-  my $timeout = defined $options{timeout} ? $options{timeout} : DEFAULT_TIMEOUT;
-  $self->{ua}->timeout($timeout);
+    my $timeout =
+      defined $options{timeout} ? $options{timeout} : DEFAULT_TIMEOUT;
+    $self->{ua}->timeout($timeout);
 
-  $self->{message} = '';
+    $self->{message} = '';
 
-  bless $self, $pkg;
-  
-  my $country_file_path = undef;
-  my $eu_country_file_path = undef;
-  my $countries_flags_file_path = undef;
-  my $countries_currencies_file_path = undef;
-  my $continent_file_path = undef;
-  if (defined $options{countries}){
-    $country_file_path = $options{countries};
-  }else{
-    $country_file_path = dist_file('Geo-IPinfo', DEFAULT_COUNTRY_FILE);
-  }
-  if (defined $options{eu_countries}){
-    $eu_country_file_path = $options{eu_countries};
-  }else{
-    $eu_country_file_path = dist_file('Geo-IPinfo', DEFAULT_EU_COUNTRY_FILE);
-  }
-  if (defined $options{countries_flags}){
-    $countries_flags_file_path = $options{countries_flags};
-  }else{
-    $countries_flags_file_path = dist_file('Geo-IPinfo', DEFAULT_COUNTRY_FLAG_FILE);
-  }
-  if (defined $options{countries_currencies}){
-    $countries_currencies_file_path = $options{countries_currencies};
-  }else{
-    $countries_currencies_file_path = dist_file('Geo-IPinfo', DEFAULT_COUNTRY_CURRENCY_FILE);
-  }
-  if (defined $options{continents}){
-    $continent_file_path = $options{continents};
-  }else{
-    $continent_file_path = dist_file('Geo-IPinfo', DEFAULT_CONTINENT_FILE);
-  }
-  $self->{countries} = $self->_read_json($country_file_path);
-  $self->{eu_countries} = $self->_read_json($eu_country_file_path);
-  $self->{countries_flags} = $self->_read_json($countries_flags_file_path);
-  $self->{countries_currencies} = $self->_read_json($countries_currencies_file_path);
-  $self->{continents} = $self->_read_json($continent_file_path);
-  $self->{cache} = $self->_build_cache(%options);
+    bless $self, $pkg;
 
-  return $self;
+    my $country_file_path              = undef;
+    my $eu_country_file_path           = undef;
+    my $countries_flags_file_path      = undef;
+    my $countries_currencies_file_path = undef;
+    my $continent_file_path            = undef;
+    if ( defined $options{countries} ) {
+        $country_file_path = $options{countries};
+    }
+    else {
+        $country_file_path = dist_file( 'Geo-IPinfo', DEFAULT_COUNTRY_FILE );
+    }
+    if ( defined $options{eu_countries} ) {
+        $eu_country_file_path = $options{eu_countries};
+    }
+    else {
+        $eu_country_file_path =
+          dist_file( 'Geo-IPinfo', DEFAULT_EU_COUNTRY_FILE );
+    }
+    if ( defined $options{countries_flags} ) {
+        $countries_flags_file_path = $options{countries_flags};
+    }
+    else {
+        $countries_flags_file_path =
+          dist_file( 'Geo-IPinfo', DEFAULT_COUNTRY_FLAG_FILE );
+    }
+    if ( defined $options{countries_currencies} ) {
+        $countries_currencies_file_path = $options{countries_currencies};
+    }
+    else {
+        $countries_currencies_file_path =
+          dist_file( 'Geo-IPinfo', DEFAULT_COUNTRY_CURRENCY_FILE );
+    }
+    if ( defined $options{continents} ) {
+        $continent_file_path = $options{continents};
+    }
+    else {
+        $continent_file_path =
+          dist_file( 'Geo-IPinfo', DEFAULT_CONTINENT_FILE );
+    }
+    $self->{countries}       = $self->_read_json($country_file_path);
+    $self->{eu_countries}    = $self->_read_json($eu_country_file_path);
+    $self->{countries_flags} = $self->_read_json($countries_flags_file_path);
+    $self->{countries_currencies} =
+      $self->_read_json($countries_currencies_file_path);
+    $self->{continents} = $self->_read_json($continent_file_path);
+    $self->{cache}      = $self->_build_cache(%options);
+
+    return $self;
 }
 
 #-------------------------------------------------------------------------------
 
-sub info
-{
-  my ($self, $ip) = @_;
+sub info {
+    my ( $self, $ip ) = @_;
 
-  return $self->_get_info($ip, '');
+    return $self->_get_info( $ip, '' );
 }
 
 #-------------------------------------------------------------------------------
 
-sub geo
-{
-  my ($self, $ip) = @_;
+sub geo {
+    my ( $self, $ip ) = @_;
 
-  return $self->_get_info($ip, 'geo');
+    return $self->_get_info( $ip, 'geo' );
 }
 
 #-------------------------------------------------------------------------------
 
-sub field
-{
-  my ($self, $ip, $field) = @_;
+sub field {
+    my ( $self, $ip, $field ) = @_;
 
-  if (not defined $field)
-  {
-    $self->{message} = 'Field must be defined.';
-    return;
-  }
+    if ( not defined $field ) {
+        $self->{message} = 'Field must be defined.';
+        return;
+    }
 
-  if (not defined $valid_fields{$field})
-  {
-    $self->{message} = "Invalid field: $field";
-    return;
-  }
+    if ( not defined $valid_fields{$field} ) {
+        $self->{message} = "Invalid field: $field";
+        return;
+    }
 
-  return $self->_get_info($ip, $field);
+    return $self->_get_info( $ip, $field );
 }
 
 #-------------------------------------------------------------------------------
 
-sub error_msg
-{
-  my $self = shift;
+sub error_msg {
+    my $self = shift;
 
-  return $self->{message};
+    return $self->{message};
 }
 
 #-------------------------------------------------------------------------------
 #-- private method(s) below , don't call them directly -------------------------
 
-sub _get_info
-{
-  my ($self, $ip, $field) = @_;
+sub _get_info {
+    my ( $self, $ip, $field ) = @_;
 
-  $ip = defined $ip ? $ip : '';
-  $field = defined $field ? $field : '';
+    $ip    = defined $ip    ? $ip    : '';
+    $field = defined $field ? $field : '';
 
-  my ($info, $message) = $self->_lookup_info($ip, $field);
-  $self->{message} = $message;
+    my ( $info, $message ) = $self->_lookup_info( $ip, $field );
+    $self->{message} = $message;
 
-  return defined $info ? Geo::Details->new($info, $field) : undef;
+    return defined $info ? Geo::Details->new( $info, $field ) : undef;
 }
 
-sub _lookup_info
-{
-  my ($self, $ip, $field) = @_;
+sub _lookup_info {
+    my ( $self, $ip, $field ) = @_;
 
-  # checking bogon IP and returning response locally.
-  if (_is_bogon($ip)) {
-    my $details = {};
-    $details->{ip} = $ip;
-    $details->{bogon} = "True";
-    return ($details, '');
-  }
-
-  my $key = $ip . '/' . $field;
-  my $cached_info = $self->_lookup_info_from_cache($key);
-
-  if (defined $cached_info)
-  {
-    return ($cached_info, '');
-  }
-
-  my ($source_info, $message) = $self->_lookup_info_from_source($key);
-  if (not defined $source_info)
-  {
-    return ($source_info, $message);
-  }
-  
-  if (ref($source_info) eq '') {
-    return ($source_info, $message);
-  }
-
-  my $country = $source_info->{country};
-  if (defined $country)
-  {
-    $source_info->{country_name} = $self->{countries}->{$country};
-    $source_info->{country_flag} = $self->{countries_flags}->{$country};
-    $source_info->{country_flag_url} = $country_flag_url.$country.".svg";
-    $source_info->{country_currency} = $self->{countries_currencies}->{$country};
-    $source_info->{continent} = $self->{continents}->{$country};
-    if ( $country ~~ $self->{eu_countries} ){
-      $source_info->{is_eu} = "True";
-    }else {
-      $source_info->{is_eu} = undef;
-    }
-  }
-
-  if (defined $source_info->{'loc'})
-  {
-    my ($lat, $lon) = split /,/, $source_info->{loc};
-    $source_info->{latitude} = $lat;
-    $source_info->{longitude} = $lon;
-  }
-
-  $source_info->{meta} = {time => time(), from_cache => 0};
-  $self->{cache}->set($key, $source_info);
-
-  return ($source_info, $message);
-}
-
-sub _lookup_info_from_cache
-{
-  my ($self, $cache_key) = @_;
-
-  my $cached_info = $self->{cache}->get($cache_key);
-  if (defined $cached_info)
-  {
-    my $timedelta = time() - $cached_info->{meta}->{time};
-    if ($timedelta <= $cache_ttl || $custom_cache == 1)
-    {
-      $cached_info->{meta}->{from_cache} = 1;
-
-      return $cached_info;
-    }
-  }
-
-  return;
-}
-
-sub _lookup_info_from_source
-{
-  my ($self, $key) = @_;
-
-  my $url = $self->{base_url} . $key;
-  my $response = $self->{ua}->get($url);
-
-  if ($response->is_success)
-  {
-   
-    my $content_type = $response->header('Content-Type') || '';
-    my $info;
-
-    if ($content_type =~ m{application/json}i) {
-      eval { $info = from_json($response->decoded_content); };
-      if ($@) {
-          return (undef, 'Error parsing JSON response.');
-      }
-    } else {
-        $info = $response->decoded_content;
-        chomp($info);
+    # checking bogon IP and returning response locally.
+    if ( _is_bogon($ip) ) {
+        my $details = {};
+        $details->{ip}    = $ip;
+        $details->{bogon} = "True";
+        return ( $details, '' );
     }
 
-    return ($info, '');
-  }
-  if ($response->code == HTTP_TOO_MANY_REQUEST)
-  {
-    return (undef, 'Your monthly request quota has been exceeded.');
-  }
+    my $key         = $ip . '/' . $field;
+    my $cached_info = $self->_lookup_info_from_cache($key);
 
-  return (undef, $response->status_line);
+    if ( defined $cached_info ) {
+        return ( $cached_info, '' );
+    }
+
+    my ( $source_info, $message ) = $self->_lookup_info_from_source($key);
+    if ( not defined $source_info ) {
+        return ( $source_info, $message );
+    }
+
+    if ( ref($source_info) eq '' ) {
+        return ( $source_info, $message );
+    }
+
+    my $country = $source_info->{country};
+    if ( defined $country ) {
+        $source_info->{country_name} = $self->{countries}->{$country};
+        $source_info->{country_flag} = $self->{countries_flags}->{$country};
+        $source_info->{country_flag_url} =
+          $country_flag_url . $country . ".svg";
+        $source_info->{country_currency} =
+          $self->{countries_currencies}->{$country};
+        $source_info->{continent} = $self->{continents}->{$country};
+        if ( $country ~~ $self->{eu_countries} ) {
+            $source_info->{is_eu} = "True";
+        }
+        else {
+            $source_info->{is_eu} = undef;
+        }
+    }
+
+    if ( defined $source_info->{'loc'} ) {
+        my ( $lat, $lon ) = split /,/, $source_info->{loc};
+        $source_info->{latitude}  = $lat;
+        $source_info->{longitude} = $lon;
+    }
+
+    $source_info->{meta} = { time => time(), from_cache => 0 };
+    $self->{cache}->set( $key, $source_info );
+
+    return ( $source_info, $message );
 }
 
-sub _read_json
-{
-  my ($pkg, $file) = @_;
+sub _lookup_info_from_cache {
+    my ( $self, $cache_key ) = @_;
 
-  my $json_text = do {
-    open my $fh, '<', $file or die "Could not open file: $file $!\n";
-    local $/;
-    <$fh>;
-  };
+    my $cached_info = $self->{cache}->get($cache_key);
+    if ( defined $cached_info ) {
+        my $timedelta = time() - $cached_info->{meta}->{time};
+        if ( $timedelta <= $cache_ttl || $custom_cache == 1 ) {
+            $cached_info->{meta}->{from_cache} = 1;
 
-  return decode_json($json_text);
+            return $cached_info;
+        }
+    }
+
+    return;
 }
 
-sub _build_cache
-{
-  my ($pkg, %options) = @_;
+sub _lookup_info_from_source {
+    my ( $self, $key ) = @_;
 
-  if (defined $options{cache})
-  {
-    $custom_cache = 1;
+    my $url      = $self->{base_url} . $key;
+    my $response = $self->{ua}->get($url);
 
-    return $options{cache};
-  }
+    if ( $response->is_success ) {
 
-  $cache_ttl = DEFAULT_CACHE_TTL;
-  if (defined $options{cache_ttl})
-  {
-      $cache_ttl = $options{cache_ttl};
-  }
+        my $content_type = $response->header('Content-Type') || '';
+        my $info;
 
-  return Cache::LRU->new(
-    size => defined $options{cache_max_size} ?
-      $options{cache_max_size} : DEFAULT_CACHE_MAX_SIZE
-  );
+        if ( $content_type =~ m{application/json}i ) {
+            eval { $info = from_json( $response->decoded_content ); };
+            if ($@) {
+                return ( undef, 'Error parsing JSON response.' );
+            }
+        }
+        else {
+            $info = $response->decoded_content;
+            chomp($info);
+        }
+
+        return ( $info, '' );
+    }
+    if ( $response->code == HTTP_TOO_MANY_REQUEST ) {
+        return ( undef, 'Your monthly request quota has been exceeded.' );
+    }
+
+    return ( undef, $response->status_line );
+}
+
+sub _read_json {
+    my ( $pkg, $file ) = @_;
+
+    my $json_text = do {
+        open my $fh, '<', $file or die "Could not open file: $file $!\n";
+        local $/;
+        <$fh>;
+    };
+
+    return decode_json($json_text);
+}
+
+sub _build_cache {
+    my ( $pkg, %options ) = @_;
+
+    if ( defined $options{cache} ) {
+        $custom_cache = 1;
+
+        return $options{cache};
+    }
+
+    $cache_ttl = DEFAULT_CACHE_TTL;
+    if ( defined $options{cache_ttl} ) {
+        $cache_ttl = $options{cache_ttl};
+    }
+
+    return Cache::LRU->new(
+        size => defined $options{cache_max_size}
+        ? $options{cache_max_size}
+        : DEFAULT_CACHE_MAX_SIZE
+    );
 }
 
 # List of bogon CIDRs.
 my @bogon_networks = (
-  "0.0.0.0/8",
-  "10.0.0.0/8",
-  "100.64.0.0/10",
-  "127.0.0.0/8",
-  "169.254.0.0/16",
-  "172.16.0.0/12",
-  "192.0.0.0/24",
-  "192.0.2.0/24",
-  "192.168.0.0/16",
-  "198.18.0.0/15",
-  "198.51.100.0/24",
-  "203.0.113.0/24",
-  "224.0.0.0/4",
-  "240.0.0.0/4",
-  "255.255.255.255/32",
-  "0:0:0:0:0:0:0:0/128", 
-  "0:0:0:0:0:0:0:1/128",
-  "0:0:0:0:0:ffff:0:0/96", 
-  "0:0:0:0:0:0:0:0/96",
-  "100::/64",
-  "2001:10::/28",
-  "2001:db8::/32",
-  "fc00::/7",
-  "fe80::/10",
-  "fec0::/10",
-  "ff00::/8",
-  "2002::/24",
-  "2002:a00::/24",
-  "2002:7f00::/24",
-  "2002:a9fe::/32",
-  "2002:ac10::/28",
-  "2002:c000::/40",
-  "2002:c000:200::/40",
-  "2002:c0a8::/32",
-  "2002:c612::/31",
-  "2002:c633:6400::/40",
-  "2002:cb00:7100::/40",
-  "2002:e000::/20",
-  "2002:f000::/20",
-  "2002:ffff:ffff::/48",
-  "2001::/40",
-  "2001:0:a00::/40",
-  "2001:0:7f00::/40",
-  "2001:0:a9fe::/48",
-  "2001:0:ac10::/44",
-  "2001:0:c000::/56",
-  "2001:0:c000:200::/56",
-  "2001:0:c0a8::/48",
-  "2001:0:c612::/47",
-  "2001:0:c633:6400::/56",
-  "2001:0:cb00:7100::/56",
-  "2001:0:e000::/36",
-  "2001:0:f000::/36",
-  "2001:0:ffff:ffff::/64"
+    "0.0.0.0/8",             "10.0.0.0/8",
+    "100.64.0.0/10",         "127.0.0.0/8",
+    "169.254.0.0/16",        "172.16.0.0/12",
+    "192.0.0.0/24",          "192.0.2.0/24",
+    "192.168.0.0/16",        "198.18.0.0/15",
+    "198.51.100.0/24",       "203.0.113.0/24",
+    "224.0.0.0/4",           "240.0.0.0/4",
+    "255.255.255.255/32",    "0:0:0:0:0:0:0:0/128",
+    "0:0:0:0:0:0:0:1/128",   "0:0:0:0:0:ffff:0:0/96",
+    "0:0:0:0:0:0:0:0/96",    "100::/64",
+    "2001:10::/28",          "2001:db8::/32",
+    "fc00::/7",              "fe80::/10",
+    "fec0::/10",             "ff00::/8",
+    "2002::/24",             "2002:a00::/24",
+    "2002:7f00::/24",        "2002:a9fe::/32",
+    "2002:ac10::/28",        "2002:c000::/40",
+    "2002:c000:200::/40",    "2002:c0a8::/32",
+    "2002:c612::/31",        "2002:c633:6400::/40",
+    "2002:cb00:7100::/40",   "2002:e000::/20",
+    "2002:f000::/20",        "2002:ffff:ffff::/48",
+    "2001::/40",             "2001:0:a00::/40",
+    "2001:0:7f00::/40",      "2001:0:a9fe::/48",
+    "2001:0:ac10::/44",      "2001:0:c000::/56",
+    "2001:0:c000:200::/56",  "2001:0:c0a8::/48",
+    "2001:0:c612::/47",      "2001:0:c633:6400::/56",
+    "2001:0:cb00:7100::/56", "2001:0:e000::/36",
+    "2001:0:f000::/36",      "2001:0:ffff:ffff::/64"
 );
 
 # Check if an IP address is a bogon.
 sub _is_bogon {
 
-  my $ip = shift;
-  my $bogon_cidr_set = Net::CIDR::Lite->new;
-  $bogon_cidr_set->add(@bogon_networks);
+    my $ip             = shift;
+    my $bogon_cidr_set = Net::CIDR::Lite->new;
+    $bogon_cidr_set->add(@bogon_networks);
 
-  return $bogon_cidr_set->find($ip);
+    return $bogon_cidr_set->find($ip);
 }
 
 #-------------------------------------------------------------------------------
