aa7a8f27644c64e409f77368d43ac793f3ec09b6
brian d foy <briandfoy@pobox.com>
2025-03-08 00:25:00 -0500

    bump version

--- Geo-IPinfo/lib/Geo/IPinfo.pm
+++ Geo-IPinfo/lib/Geo/IPinfo.pm
@@ -11,7 +11,7 @@ use Geo::Details;
 use Net::CIDR;
 use Net::CIDR::Set;
 
-our $VERSION = '3.0.1';
+our $VERSION = '3.0.2';
 use constant DEFAULT_CACHE_MAX_SIZE => 4096;
 use constant DEFAULT_CACHE_TTL      => 86_400;
 use constant DEFAULT_TIMEOUT        => 2;
