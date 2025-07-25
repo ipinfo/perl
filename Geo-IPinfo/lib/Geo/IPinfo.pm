233587bcdbc1f8be66e155a1ca744eccc59d6680
Maxime Curioni <maxime@ipinfo.io>
2025-07-24 19:13:15 -0600

    3.1.1 release (mark as latest)

--- Geo-IPinfo/lib/Geo/IPinfo.pm
+++ Geo-IPinfo/lib/Geo/IPinfo.pm
@@ -11,7 +11,7 @@ use Geo::Details;
 use Net::CIDR;
 use Net::CIDR::Set;
 
-our $VERSION = '3.1.0';
+our $VERSION = '3.1.1';
 use constant DEFAULT_CACHE_MAX_SIZE => 4096;
 use constant DEFAULT_CACHE_TTL      => 86_400;
 use constant DEFAULT_TIMEOUT        => 2;
@@ -1389,7 +1389,7 @@ Geo::IPinfo -  The official Perl library for IPinfo.
 
 =head1 VERSION
 
-Version 3.0.1
+Version 3.1.1
 
 =cut
 
